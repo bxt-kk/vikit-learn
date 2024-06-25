7dcbfa421c49c563daa0bb4f7f64c35881cfea74
bxt-kk <shaypatellisiod78@gmail.com>
2024-06-25 10:08:02 +0800

    all in native ln

--- vklearn/models/component.py
+++ vklearn/models/component.py
@@ -6,35 +6,35 @@ import torch.nn as nn
 from torchvision.ops.misc import SqueezeExcitation
 
 
-DEFAULT_LAYER_NORM = nn.BatchNorm2d
+class LayerNorm2d(nn.GroupNorm):
+
+    def __init__(self, num_channels:int):
+        super().__init__(1, num_channels)
+
+
+# class LayerNorm2d(nn.Module):
+#     def __init__(
+#             self,
+#             num_channels: int,
+#             eps:          float=1e-5,
+#         ):
+#
+#         super().__init__()
+#         self.weight = nn.Parameter(torch.ones(num_channels))
+#         self.bias = nn.Parameter(torch.zeros(num_channels))
+#         self.eps = eps
+#
+#     def forward(self, x: torch.Tensor) -> torch.Tensor:
+#         u = x.mean(1, keepdim=True)
+#         s = (x - u).pow(2).mean(1, keepdim=True)
+#         x = (x - u) / torch.sqrt(s + self.eps)
+#         x = self.weight[:, None, None] * x + self.bias[:, None, None]
+#         return x
+
+
+DEFAULT_LAYER_NORM = LayerNorm2d # nn.BatchNorm2d
 DEFAULT_ACTIVATION = nn.Hardswish
 DEFAULT_SIGMOID    = nn.Hardsigmoid
-
-
-# class LayerNorm2d(nn.GroupNorm):
-#
-#     def __init__(self, num_channels:int):
-#         super().__init__(1, num_channels)
-
-
-class LayerNorm2d(nn.Module):
-    def __init__(
-            self,
-            num_channels: int,
-            eps:          float=1e-5,
-        ):
-
-        super().__init__()
-        self.weight = nn.Parameter(torch.ones(num_channels))
-        self.bias = nn.Parameter(torch.zeros(num_channels))
-        self.eps = eps
-
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        u = x.mean(1, keepdim=True)
-        s = (x - u).pow(2).mean(1, keepdim=True)
-        x = (x - u) / torch.sqrt(s + self.eps)
-        x = self.weight[:, None, None] * x + self.bias[:, None, None]
-        return x
 
 
 class LocalSqueezeExcitation(nn.Module):
