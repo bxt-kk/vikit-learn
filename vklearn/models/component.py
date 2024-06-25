e5bf15b4f60bc1f9b6080aa2efd12d057a39d7ce
bxt-kk <shaypatellisiod78@gmail.com>
2024-06-25 09:36:08 +0800

    custom layernorm2d

--- vklearn/models/component.py
+++ vklearn/models/component.py
@@ -11,10 +11,30 @@ DEFAULT_ACTIVATION = nn.Hardswish
 DEFAULT_SIGMOID    = nn.Hardsigmoid
 
 
-class LayerNorm2d(nn.GroupNorm):
+# class LayerNorm2d(nn.GroupNorm):
+#
+#     def __init__(self, num_channels:int):
+#         super().__init__(1, num_channels)
 
-    def __init__(self, num_channels:int):
-        super().__init__(1, num_channels)
+
+class LayerNorm2d(nn.Module):
+    def __init__(
+            self,
+            num_channels: int,
+            eps:          float=1e-5,
+        ):
+
+        super().__init__()
+        self.weight = nn.Parameter(torch.ones(num_channels))
+        self.bias = nn.Parameter(torch.zeros(num_channels))
+        self.eps = eps
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        u = x.mean(1, keepdim=True)
+        s = (x - u).pow(2).mean(1, keepdim=True)
+        x = (x - u) / torch.sqrt(s + self.eps)
+        x = self.weight[:, None, None] * x + self.bias[:, None, None]
+        return x
 
 
 class LocalSqueezeExcitation(nn.Module):
