d2a4c6d70e9682ee9bd13f3a2eb228b7defdf93e
bxt-kk <shaypatellisiod78@gmail.com>
2024-08-07 11:23:50 +0800

    reset default param

--- vklearn/models/trimnetdet_alpha.py
+++ vklearn/models/trimnetdet_alpha.py
@@ -226,7 +226,7 @@ class TrimNetDet(Detector):
             self,
             index: List[Tensor],
             xyxys: Tensor,
-            scale: float=0.5,
+            scale: float=0.33,
         ) -> List[Tensor]:
 
         if index[0].shape[0] == 0: return index
