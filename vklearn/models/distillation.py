7005c1ca6a48fb76f090cb899c0d75a8bceee544
bxt-kk <shaypatellisiod78@gmail.com>
2024-08-21 17:21:18 +0800

    update distillation

--- vklearn/models/distillation.py
+++ vklearn/models/distillation.py
@@ -4,6 +4,7 @@ from torch import Tensor
 
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
 from torchvision import tv_tensors
 from torchvision.transforms import v2
@@ -17,42 +18,63 @@ class Distillation(Basic):
 
     def __init__(
             self,
-            student: nn.Module,
             teacher: nn.Module,
+            student: nn.Module,
+            project: nn.Module,
         ):
 
         super().__init__()
 
-        self.student = student
         self.teacher = teacher
+        self.student = student
+        self.project = project
 
         self.mse_metric = MeanSquaredError()
         self.mae_metric = MeanAbsoluteError()
 
+    def align_inputs(self, x:Tensor) -> Tensor:
+        return x
+
+    def forward(self, x:Tensor) -> Tuple[Tensor, Tensor]:
+        with torch.no_grad():
+            teacher_targ = self.teacher(self.align_inputs(x))
+        student_pred = self.project(self.student(x))
+        return student_pred, teacher_targ
+
     def calc_loss(
             self,
-            inputs:  Tensor,
+            inputs:  Tuple[Tensor, Tensor],
             target:  Tensor,
-            weights: Dict[str, float] | None=None,
         ) -> Dict[str, Any]:
-        assert not 'this is an empty func'
+
+        teacher_targ, student_pred = inputs
+        loss = F.mse_loss(student_pred, teacher_targ, reduction='mean')
+        return dict(
+            loss=loss,
+        )
 
     def calc_score(
             self,
-            inputs: Tensor,
+            inputs: Tuple[Tensor, Tensor],
             target: Tensor,
             eps:    float=1e-5,
         ) -> Dict[str, Any]:
-        assert not 'this is an empty func'
+
+        teacher_targ, student_pred = inputs
+        cos_sim = F.cosine_similarity(student_pred, teacher_targ, dim=1).mean()
+        return dict(
+            cos_sim=cos_sim,
+        )
 
     def update_metric(
             self,
-            inputs: Tensor,
+            inputs: Tuple[Tensor, Tensor],
             target: Tensor,
         ):
 
-        self.mse_metric.update(inputs, target)
-        self.mae_metric.update(inputs, target)
+        teacher_targ, student_pred = inputs
+        self.mse_metric.update(student_pred, teacher_targ)
+        self.mae_metric.update(student_pred, teacher_targ)
 
     def compute_metric(self) -> Dict[str, Any]:
         mse = self.mse_metric.compute()
@@ -82,178 +104,56 @@ class Distillation(Basic):
         test_transforms  = None
 
         if task_name in ('default', 'cocox512'):
-            train_transforms = v2.Compose([
-                v2.ToImage(),
-                v2.ScaleJitter(
-                    target_size=(512, 512),
-                    scale_range=(384 / 512, 1.1),
-                    antialias=True),
-                v2.RandomPhotometricDistort(p=1),
-                v2.RandomHorizontalFlip(p=0.5),
-                v2.RandomChoice([
-                    v2.GaussianBlur(7, sigma=(0.1, 2.0)),
-                    v2.RandomAdjustSharpness(2, p=0.5),
-                    v2.RandomEqualize(p=0.5),
-                ]),
-                v2.RandomCrop(
-                    size=(512, 512),
-                    pad_if_needed=True,
-                    fill={tv_tensors.Image: 127, tv_tensors.Mask: 0}),
-                v2.SanitizeBoundingBoxes(min_size=3),
-                v2.ToDtype(torch.float32, scale=True),
-                v2.Normalize(
-                    mean=[0.485, 0.456, 0.406],
-                    std=[0.229, 0.224, 0.225],
-                )
-            ])
-            test_transforms = v2.Compose([
-                v2.ToImage(),
-                v2.Resize(
-                    size=511,
-                    max_size=512,
-                    antialias=True),
-                v2.Pad(
-                    padding=512 // 4,
-                    fill={tv_tensors.Image: 127, tv_tensors.Mask: 0}),
-                v2.CenterCrop(512),
-                v2.SanitizeBoundingBoxes(min_size=3),
-                v2.ToDtype(torch.float32, scale=True),
-                v2.Normalize(
-                    mean=[0.485, 0.456, 0.406],
-                    std=[0.229, 0.224, 0.225],
-                )
-            ])
-
+            aligned_size = 512
         elif task_name == 'cocox384':
-            train_transforms = v2.Compose([
-                v2.ToImage(),
-                v2.ScaleJitter(
-                    target_size=(384, 384),
-                    scale_range=(0.9, 1.1),
-                    antialias=True),
-                v2.RandomPhotometricDistort(p=1),
-                v2.RandomHorizontalFlip(p=0.5),
-                v2.RandomChoice([
-                    v2.GaussianBlur(7, sigma=(0.1, 2.0)),
-                    v2.RandomAdjustSharpness(2, p=0.5),
-                    v2.RandomEqualize(p=0.5),
-                ]),
-                v2.RandomCrop(
-                    size=(384, 384),
-                    pad_if_needed=True,
-                    fill={tv_tensors.Image: 127, tv_tensors.Mask: 0}),
-                v2.SanitizeBoundingBoxes(min_size=3),
-                v2.ToDtype(torch.float32, scale=True),
-                v2.Normalize(
-                    mean=[0.485, 0.456, 0.406],
-                    std=[0.229, 0.224, 0.225],
-                )
-            ])
-            test_transforms = v2.Compose([
-                v2.ToImage(),
-                v2.Resize(
-                    size=383,
-                    max_size=384,
-                    antialias=True),
-                v2.Pad(
-                    padding=384 // 4,
-                    fill={tv_tensors.Image: 127, tv_tensors.Mask: 0}),
-                v2.CenterCrop(384),
-                v2.SanitizeBoundingBoxes(min_size=3),
-                v2.ToDtype(torch.float32, scale=True),
-                v2.Normalize(
-                    mean=[0.485, 0.456, 0.406],
-                    std=[0.229, 0.224, 0.225],
-                )
-            ])
-
+            aligned_size = 384
         elif task_name == 'cocox448':
-            train_transforms = v2.Compose([
-                v2.ToImage(),
-                v2.ScaleJitter(
-                    target_size=(448, 448),
-                    scale_range=(384 / 448, 1.1),
-                    antialias=True),
-                v2.RandomPhotometricDistort(p=1),
-                v2.RandomHorizontalFlip(p=0.5),
-                v2.RandomChoice([
-                    v2.GaussianBlur(7, sigma=(0.1, 2.0)),
-                    v2.RandomAdjustSharpness(2, p=0.5),
-                    v2.RandomEqualize(p=0.5),
-                ]),
-                v2.RandomCrop(
-                    size=(448, 448),
-                    pad_if_needed=True,
-                    fill={tv_tensors.Image: 127, tv_tensors.Mask: 0}),
-                v2.SanitizeBoundingBoxes(min_size=3),
-                v2.ToDtype(torch.float32, scale=True),
-                v2.Normalize(
-                    mean=[0.485, 0.456, 0.406],
-                    std=[0.229, 0.224, 0.225],
-                )
-            ])
-            test_transforms = v2.Compose([
-                v2.ToImage(),
-                v2.Resize(
-                    size=447,
-                    max_size=448,
-                    antialias=True),
-                v2.Pad(
-                    padding=448 // 4,
-                    fill={tv_tensors.Image: 127, tv_tensors.Mask: 0}),
-                v2.CenterCrop(448),
-                v2.SanitizeBoundingBoxes(min_size=3),
-                v2.ToDtype(torch.float32, scale=True),
-                v2.Normalize(
-                    mean=[0.485, 0.456, 0.406],
-                    std=[0.229, 0.224, 0.225],
-                )
-            ])
-
+            aligned_size = 448
         elif task_name == 'cocox640':
-            train_transforms = v2.Compose([
-                v2.ToImage(),
-                v2.ScaleJitter(
-                    target_size=(640, 640),
-                    scale_range=(384 / 640, 1.1),
-                    antialias=True),
-                v2.RandomPhotometricDistort(p=1),
-                v2.RandomHorizontalFlip(p=0.5),
-                v2.RandomChoice([
-                    v2.GaussianBlur(7, sigma=(0.1, 2.0)),
-                    v2.RandomAdjustSharpness(2, p=0.5),
-                    v2.RandomEqualize(p=0.5),
-                ]),
-                v2.RandomCrop(
-                    size=(640, 640),
-                    pad_if_needed=True,
-                    fill={tv_tensors.Image: 127, tv_tensors.Mask: 0}),
-                v2.SanitizeBoundingBoxes(min_size=3),
-                v2.ToDtype(torch.float32, scale=True),
-                v2.Normalize(
-                    mean=[0.485, 0.456, 0.406],
-                    std=[0.229, 0.224, 0.225],
-                )
-            ])
-            test_transforms = v2.Compose([
-                v2.ToImage(),
-                v2.Resize(
-                    size=639,
-                    max_size=640,
-                    antialias=True),
-                v2.Pad(
-                    padding=640 // 4,
-                    fill={tv_tensors.Image: 127, tv_tensors.Mask: 0}),
-                v2.CenterCrop(640),
-                v2.SanitizeBoundingBoxes(min_size=3),
-                v2.ToDtype(torch.float32, scale=True),
-                v2.Normalize(
-                    mean=[0.485, 0.456, 0.406],
-                    std=[0.229, 0.224, 0.225],
-                )
-            ])
-
+            aligned_size = 640
         else:
             raise ValueError(f'Unsupported the task `{task_name}`')
+
+        train_transforms = v2.Compose([
+            v2.ToImage(),
+            v2.ScaleJitter(
+                target_size=(aligned_size, aligned_size),
+                scale_range=(min(0.9, 384 / aligned_size), 1.1),
+                antialias=True),
+            v2.RandomPhotometricDistort(p=1),
+            v2.RandomHorizontalFlip(p=0.5),
+            v2.RandomChoice([
+                v2.GaussianBlur(7, sigma=(0.1, 2.0)),
+                v2.RandomAdjustSharpness(2, p=0.5),
+                v2.RandomEqualize(p=0.5),
+            ]),
+            v2.RandomCrop(
+                size=(aligned_size, aligned_size),
+                pad_if_needed=True,
+                fill={tv_tensors.Image: 127, tv_tensors.Mask: 0}),
+            v2.SanitizeBoundingBoxes(min_size=3),
+            v2.ToDtype(torch.float32, scale=True),
+            v2.Normalize(
+                mean=[0.485, 0.456, 0.406],
+                std=[0.229, 0.224, 0.225],
+            )
+        ])
+        test_transforms = v2.Compose([
+            v2.ToImage(),
+            v2.Resize(
+                size=aligned_size - 1,
+                max_size=aligned_size,
+                antialias=True),
+            v2.Pad(
+                padding=aligned_size // 4,
+                fill={tv_tensors.Image: 127, tv_tensors.Mask: 0}),
+            v2.CenterCrop(aligned_size),
+            v2.SanitizeBoundingBoxes(min_size=3),
+            v2.ToDtype(torch.float32, scale=True),
+            v2.Normalize(
+                mean=[0.485, 0.456, 0.406],
+                std=[0.229, 0.224, 0.225],
+            )
+        ])
 
         return train_transforms, test_transforms
