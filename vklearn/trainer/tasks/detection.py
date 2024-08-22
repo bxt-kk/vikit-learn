11675efbe08991ac42c8a37b242507c03c78101e
bxt-kk <shaypatellisiod78@gmail.com>
2024-08-22 13:27:21 +0800

    add doc

--- vklearn/trainer/tasks/detection.py
+++ vklearn/trainer/tasks/detection.py
@@ -7,6 +7,20 @@ from ...models.detector import Detector as Model
 
 @dataclass
 class Detection(Task):
+    '''This `class` is used to configure a set of parameters relevant to a specific task in object detector model training. 
+
+    Args:
+        model: Specify a object detection model object.
+        device: Computation device supported by PyTorch.
+        metric_start_epoch: Sets the epoch from which metric calculation starts, defaults to 0.
+        fit_features_start: Sets the epoch from which feature extractor training starts, -1 means no training, defaults to -1.
+        loss_options: Set optional parameters for the object detection model's loss function.
+        score_options: Set optional parameters for the object detection model's scoring function.
+        metric_options: Set optional parameters for the object detection model's metric evaluation function.
+        key_metrics: Specifies which key evaluation metrics to track.
+        best_metric: Current best metric score, initialized to 0.
+    '''
+
     model:       Model
     key_metrics: Tuple[str]=('map', 'map_50', 'map_75')
 
--- vklearn/trainer/tasks/segmentation.py
+++ vklearn/trainer/tasks/segmentation.py
@@ -7,6 +7,20 @@ from ...models.segment import Segment as Model
 
 @dataclass
 class Segmentation(Task):
+    '''This `class` is used to configure a set of parameters relevant to a specific task in segmentation model training. 
+
+    Args:
+        model: Specify a segmentation model object.
+        device: Computation device supported by PyTorch.
+        metric_start_epoch: Sets the epoch from which metric calculation starts, defaults to 0.
+        fit_features_start: Sets the epoch from which feature extractor training starts, -1 means no training, defaults to -1.
+        loss_options: Set optional parameters for the segmentation model's loss function.
+        score_options: Set optional parameters for the segmentation model's scoring function.
+        metric_options: Set optional parameters for the segmentation model's metric evaluation function.
+        key_metrics: Specifies which key evaluation metrics to track.
+        best_metric: Current best metric score, initialized to 0.
+    '''
+
     model:       Model
     key_metrics: Tuple[str]=('miou', )
 
