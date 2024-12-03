b60f7e8a4dddd71d5f882eb3a4b6e46c73f74786
bxt-kk <shaypatellisiod78@gmail.com>
2024-12-03 18:47:35 +0800

    detach lstm input

--- vklearn/models/trimnetocr.py
+++ vklearn/models/trimnetocr.py
@@ -55,7 +55,7 @@ class TrimNetOcr(OCR):
         # n, c, 1, w -> n, c, w -> n, w, c
         x = x.squeeze(dim=2).transpose(1, 2)
         # print('rnn out shape:', self.rnn(x)[0].shape)
-        s, _ = self.rnn(x)
+        s, _ = self.rnn(x.detach())
         x = self.classifier(x + s)
         return x
 
